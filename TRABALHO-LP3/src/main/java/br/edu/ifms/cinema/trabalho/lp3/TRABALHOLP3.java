718c62f7c178a6781e9658eb7c58029de52e20a1
victor ferreira <victor ferreira@DESKTOP-BVTPPA3>
2025-10-20 21:43:48 -0400

    adicionando o projeto mave ao repositorio remoto no GitHub

---
+++ TRABALHO-LP3/src/main/java/br/edu/ifms/cinema/trabalho/lp3/TRABALHOLP3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifms.cinema.trabalho.lp3;
+
+/**
+ *
+ * @author victor ferreira
+ */
+public class TRABALHOLP3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
